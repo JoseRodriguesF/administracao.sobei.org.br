9e9ffe85cb14c0aef705a7cdeb3a3355af6c9a49
JoseRodriguesF <jose.antonio.220507@gmail.com>
2026-08-27 15:49:03 -0300

    style(ui): ajustar layout da pagina inicial do dashboard removendo card envolvente

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function AdminDashboardPage() {
   return (
     <div className="dashboard-home">
-      <div className="dashboard-home__card">
+      <div className="dashboard-home__content">
         <div className="dashboard-home__logo-wrapper">
           <Image
             src="/images/LOGO AZUL.png"
